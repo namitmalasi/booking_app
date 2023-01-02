1b4452e92a59af5f99a45802c9291a9c7d0a6d9e
Namit Malasi <namitmalasi@gmail.com>
2023-01-02 13:17:21 +0530

    added the featured properties components in home page

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -5,11 +5,13 @@ const Featured = () => {
   return (
     <div className="featured">
       <div className="featuredItem">
+      
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
           alt="featured pic"
           className="featuredImg"
         />
+
         <div className="featuredTitles">
           <h1>Mumbai</h1>
           <h2>123 properties</h2>
