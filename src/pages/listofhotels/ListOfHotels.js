0dcea1e4d9bbc7d8092edc9e10765e9b57207ba8
Namit Malasi <namitmalasi@gmail.com>
2023-01-03 20:35:29 +0530

    searched hotel description component

--- src/pages/listofhotels/ListOfHotels.js
+++ src/pages/listofhotels/ListOfHotels.js
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const ListOfHotels = () => {
   const location = useLocation();
@@ -93,7 +94,13 @@ const ListOfHotels = () => {
             <button>Search</button>
           </div>
 
-          <div className="listResult"></div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
         </div>
       </div>
     </div>
