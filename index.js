155d17d86a9ef02ec658b18a81b2d704c5b43075
Namit Malasi <namitmalasi@gmail.com>
2023-01-05 20:15:31 +0530

    established the mongodb connection

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.listen(8800, () => {
-  console.log("Connected to Server");
-});
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to Database");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to Server");
+});
