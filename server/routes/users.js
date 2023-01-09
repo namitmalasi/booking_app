b35b2b78fd16c2dab5a676f4376ab32af4c30e86
Namit Malasi <namitmalasi@gmail.com>
2023-01-09 20:26:47 +0530

    jwt authorization verification

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,24 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controller/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+// DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+// GET
+router.get("/:id", verifyUser, getUser);
+
+// GET ALL
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
