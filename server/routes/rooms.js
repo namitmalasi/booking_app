b388aebb938d942422fae615435b4fb9310b718d
Namit Malasi <namitmalasi@gmail.com>
2023-01-10 19:02:43 +0530

    created controller and moel for hotrl room

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,5 +1,28 @@
 import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} from "../controller/room.js";
 
 const router = express.Router();
+
+// CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+// DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// GET
+router.get("/:id", getRoom);
+
+// GET ALL
+router.get("/", getRooms);
 
 export default router;
